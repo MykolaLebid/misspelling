045b659fa54be478ffdf6c1eb15793a3f348cd0b
MykolaLebid <mykola.lebid@gmail.com>
2019-09-18 18:48:26 +0300

    refactoring misspelling.h

--- misspelling.h
+++ misspelling.h
@@ -6,20 +6,19 @@ class Word
 public:
 	const unsigned int code_length = 4;
 
-	std::string first_letter(const std::string str) const {
-		return str.substr(0, 1);
-	};
-	std::string drop_h_w(const std::string str) const;
-	char abundant_code_letter(const char ch) const;
+	std::string code_head(const std::string str) const {return str.substr(0, 1);};
 	
-	std::string abundant_code_string(const std::string str) const;
-	std::string filtered_code_string(const std::string abundant_code_str) const;
 	std::string code_tail(const std::string str) const;
+	std::string drop_h_w(const std::string str) const;
 	
+	char abundant_code_letter(const char ch) const;
+	std::string abundant_code_string(const std::string str) const;	
+	std::string filtered_code_string(const std::string abundant_code_str) const;
 	std::string zero_pad_formatting(std::string filtered_code_str) const; 
+	
 	std::string code(const std::string str) const {
 		if (str!="") {
-			return first_letter(str)  +  code_tail(str);
+			return code_head(str)  +  code_tail(str);
 		} else {
 			return "";
 		};
